1d3845b5520beb737687aa7c427ee424aa2dcf04
RobaRazzaq <robarazzaq@gmail.com>
2022-09-18 19:28:28 +0500

    Reassigne the value of integers

---
+++ tut2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 45;
+    cout << "The value of a was: " << a;
+    a = 34;
+    cout << "The value of a is: " << a;
+    return 0;
+}
---
+++ tut3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 100;
+    cout << "The value of a was: " << a;
+    a = 24;
+    cout << "The value of a is: " << a;
+    return 0;
+}
