f4cbb34aa60a3380b2265254f84a7915ffebef19
RobaRazzaq <robarazzaq@gmail.com>
2022-09-18 20:06:25 +0500

    grade file

---
+++ tut4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int grade = 90;
+    if (grade != 90)
+    {
+        cout << "Fail\n";
+    }
+    else
+    {
+        cout << "Pass\n";
+    }
+}
